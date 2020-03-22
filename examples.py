d3d4a2a8587709652a9e7eeeaafe97e5710c8a31
Kevin Li <kevintli98@gmail.com>
2020-03-22 06:56:03 -0700

    Add Gaussian example and conda env yml

--- examples.py
+++ examples.py
@@ -1,7 +1,8 @@
 from bayes_net import BayesNet
-from distributions import DiscreteDistribution
-from conditionals import TabularCPD
+from distributions import DiscreteDistribution, GaussianDistribution
+from conditionals import TabularCPD, GaussianCPD
 from collections import Counter
+import numpy as np
 import json
 
 def create_basic_example():
@@ -91,6 +92,18 @@ def create_medium_example():
     bn.build()
     return bn
 
+def create_gaussian_example():
+    # Bayes Net with factors P(A) and P(B|A).
+    # P(A) is N(0, 1), and P(B|A) is [N(A, 1)  N(2A, 4)]
+
+    bn = BayesNet(["A", "B"])
+    bn.set_prior("A", GaussianDistribution(5, 1))
+    bn.set_parents("B", ["A"], GaussianCPD(lambda a: GaussianDistribution((a, 2*a), np.array([[1, 0], [0, 4]]))))
+
+    bn.build()
+    return bn
+
+
 def sampling_example():
     """
     Samples from different Bayes nets to verify that the frequencies are what we expect
@@ -111,6 +124,9 @@ def sampling_example():
     print("\nWITH EVIDENCE\n==========")
     print_sample_freqs(bn2, 100000, evidence={"A": 0, "B": 0, "D": 0})
 
+    # Gaussian conditionals
+    print("\nGAUSSIAN CONDITIONAL PROBABILITIES\n==========")
+    print_sample_mean_cov(create_gaussian_example(), 1000)
 
 def print_sample_freqs(bn, num_samples, evidence=None):
     samples = []
@@ -120,3 +136,10 @@ def print_sample_freqs(bn, num_samples, evidence=None):
     freqs = DiscreteDistribution(Counter(samples), needs_normalize=True).probs
     for val, freq in freqs.items():
         print(f"{val}: {freq}")
+
+def print_sample_mean_cov(bn, num_samples):
+    total = np.array(bn.sample())
+    for _ in range(num_samples - 1):
+        sample = np.array(bn.sample())
+        total += sample
+    print(f"Mean: {total / num_samples}")
