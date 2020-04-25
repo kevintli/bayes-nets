Fix backprop for VI

discriminative_markov_chain.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import torch
 import torch.optim as optim
 from bayes_net import BayesNet
@@ -36,7 +37,7 @@ class DiscriminativeMarkovChain(BayesNet):
     def __init__(self, num_nodes):
         super(DiscriminativeMarkovChain, self).__init__(num_nodes)
 
-    def generate_cpds(self, degree=1, coeff_range=[-5, 5], max_sd=2):
+    def generate_cpds(self, degree=1, coeff_range=[-1, 1], max_sd=1):
         vals, covs = [], []
         for i in range(1, self.num_nodes):
             # Generate random coefficients for each degree
@@ -118,7 +119,7 @@ class DiscriminativeMarkovChain(BayesNet):
         self.build()
         return (xend_prior.mean, xend_prior.cov), parameters
 
-    def fit_ELBO(self, data, fitted_mc, curr_evidence=1):
+    def fit_ELBO(self, data, fitted_mc, curr_evidence=1, plot_name="vi_loss"):
         """
         Params
             data (list[tensor]) - A list containing values for each variable, sampled from the joint distribution
@@ -151,25 +152,30 @@ class DiscriminativeMarkovChain(BayesNet):
             for i in range(num_nodes - 1):
                 # A single sample from the variational family per X1, X2, X3, X4...
                 variational_samples.append(models[i]([evidence]).sample()[0])
+
+            # Add the evidence (X5) as the last one
             variational_samples.append(evidence)
+
+            # print(f"Sampled values from q:")
+            # print(f"X_1: {variational_samples[0]}")
+            # print(f"X_2 (evidence): {variational_samples[1]}")
 
             # Get logprobs from the fitted model
             log_probs = []
             for i in range(num_nodes):
                 node = fitted_mc.get_node(f"X_{i + 1}")
                 evidence_nodes = node.parents
-                evidence_idx = [int(e.split('_')[1]) for e in evidence_nodes]
+                evidence_idx = [int(e.split('_')[1]) - 1 for e in evidence_nodes]
                 evidences = variational_samples[evidence_idx[0]] if len(evidence_idx) > 0 else []
                 x = variational_samples[i]
-                
-                with torch.no_grad():
-                # Special case of distribution vs CPD
-                # E_q[log p(X1)p(X2|X1)p(X3|X2)p(X4|X3)p(X5|X4))] - H(q)
-                    if evidences == []:
-                        log_probs.append(torch.mean(node.cpd.get_log_probability(x)))
-                    else:
-                        log_probs.append(torch.mean(node.cpd.get_log_probability(x, [evidences])))
 
+                # print(f"For node X_{i+1}, we need evidence indices {evidence_idx}. The evidences are: {evidences}")
+                
+                # E_q[log p(X1)p(X2|X1)p(X3|X2)p(X4|X3)p(X5|X4))] - H(q)
+                if evidences == []:
+                    log_probs.append(torch.mean(node.cpd.get_log_probability(x)))
+                else:
+                    log_probs.append(torch.mean(node.cpd.get_log_probability(x, [evidences])))
 
             # Get the additional entropy terms for q -> E_q(-log q)
             # get log probs
@@ -190,9 +196,10 @@ class DiscriminativeMarkovChain(BayesNet):
         # Setting up pytorch iteration
         dataset_size = 10000
         batch_size = 32
-        num_epochs = 50
+        num_epochs = 25
         # evidence_data = curr_evidence*np.ones((dataset_size,))
         evidence_data = torch.tensor(data)[:, end_idx]
+        # print(f"The evidence data is: {evidence_data}")
         # Iterable that gives data from training set in batches with shuffling
         trainloader = torch.utils.data.DataLoader(Dataset(evidence_data), batch_size=batch_size,
                                                   shuffle=True)
@@ -204,6 +211,7 @@ class DiscriminativeMarkovChain(BayesNet):
         optimizer = optim.Adam(all_params)
 
         print("Begin training loop")
+        train_losses = []
         # Pytorch training loop
         for epoch in range(num_epochs):
             total_loss = 0
@@ -217,6 +225,11 @@ class DiscriminativeMarkovChain(BayesNet):
 
             # Print statistics
             print(f"\nEpoch {epoch}; Avg Loss: {total_loss / len(trainloader)}")  # Avg loss per batch
+            print(models[0].weights[0].weight.item(), models[0].weights[0].bias.item())
+            print(models[0].cov_matrix())
+            train_losses += [total_loss / len(trainloader)]
+            plt.plot(train_losses)
+            plt.savefig(f"{plot_name}.png")
             epoch += 1
 
         return models

fitting.py
@@ -43,7 +43,7 @@ class LinearGaussianConditionalFn(nn.Module):
         # Fixed covariance matrix (learnable), regardless of evidence
         self.cov = torch.nn.Parameter(torch.eye(data_dim) if data_dim > 1 else torch.tensor(1.))
 
-    def cov_matrix(self, with_grad=False):
+    def cov_matrix(self):
         # Scalar case: don't do anything
         if len(self.cov.shape) == 0:
             return self.cov
@@ -51,10 +51,9 @@ class LinearGaussianConditionalFn(nn.Module):
         # To ensure that covariance matrix is PD, don't use self.cov directly.
         #  Instead, let L = lower diagonal matrix with contents of self.cov,
         #  and use LL^T + eps*I as the covariance matrix.
-        with torch.set_grad_enabled(with_grad):
-            L = torch.tril(self.cov)
-            cov = L @ L.T + 1e-8 * torch.eye(L.shape[0])
-            return torch.clamp(cov, min=0)
+        L = torch.tril(self.cov)
+        cov = L @ L.T + 1e-8 * torch.eye(L.shape[0])
+        return torch.clamp(cov, min=0)
     
     def forward(self, evidence):
         """
@@ -64,7 +63,7 @@ class LinearGaussianConditionalFn(nn.Module):
             evidence (list[torch.tensor]) - A list containing values for each evidence variable
         """
         mean = sum([self.weights[i](evidence.float()) for i, evidence in enumerate(evidence)])
-        cov = self.cov_matrix(with_grad=True)
+        cov = self.cov_matrix()
 
         return GaussianDistribution(mean, cov)
 
@@ -123,7 +122,8 @@ def learn_gaussian_conditional_fn(cond_fn_approx, evidence, data, num_epochs=25,
         # Log current parameters
         a = cond_fn_approx.weights[0].weight.squeeze()
         b = cond_fn_approx.weights[0].bias.squeeze()
-        cov = cond_fn_approx.cov_matrix()
+        with torch.no_grad():
+            cov = cond_fn_approx.cov_matrix()
         if verbose:
             print(f"a: {a}\nb: {b}\nCov: {cov}")
         if log_fn:
@@ -133,10 +133,13 @@ def learn_gaussian_conditional_fn(cond_fn_approx, evidence, data, num_epochs=25,
         print(f"\nEpoch {epoch}; Avg Loss: {total_loss / len(trainloader)}") # Avg loss per batch
         epoch += 1
         total_loss = 0
+        
+    # No gradients during actual evaluation
+    for param in cond_fn_approx.parameters():
+        param.requires_grad_(False)
 
     def cond_fn(evidence):
-        with torch.no_grad():
-            distr = cond_fn_approx(evidence)
+        distr = cond_fn_approx(evidence)
         return distr
 
     return cond_fn, cond_fn_approx

fitting_experiments.py
@@ -3,6 +3,7 @@ from discriminative_markov_chain import DiscriminativeMarkovChain
 import matplotlib.pyplot as plt
 from distributions import GaussianDistribution
 import numpy as np
+import torch
 
 def print_comparison(true_params, fitted_params):
     print(f"a  | True: {true_params['coeffs'][0]}, Fitted: {fitted_params['a']}")
@@ -109,8 +110,9 @@ def test_true_linear_discriminative(mc, num_samples=10000):
 def test_true_linear_variational(mc, num_samples=10000):
     print("Testing VI")
 
-    node_errs = {}
     true_params = mc.parameters
+
+    a_errs, b_errs, sd_errs = {}, {}, {}
 
     def log_fn(node, epoch, data):
         """
@@ -121,29 +123,47 @@ def test_true_linear_variational(mc, num_samples=10000):
         a_err = (data['a'] - a_true) ** 2
         b_err = (data['b'] - b_true) ** 2
         sd_err = (data['cov'] - sd_true) ** 2
-        node_errs[node] = node_errs.get(node, []) + [(a_err.item(), b_err.item(), sd_err.item())]
+        a_errs[node] = a_errs.get(node, []) + [a_err.item()]
+        b_errs[node] = b_errs.get(node, []) + [b_err.item()]
+        sd_errs[node] = sd_errs.get(node, []) + [sd_err.item()]
 
+    # Sample from p (the true joint distribution)
     data = mc.sample_batch(num_samples)
 
-    # fitted_mc = SimpleMarkovChain(length)
-    # (x1_m, x1_sd), fitted_params = fitted_mc.fit_cpds_to_data(data, log_fn)
-
+    # Initialize a discriminative model and fit it using VI
     variational_mc = DiscriminativeMarkovChain(mc.num_nodes)
-    true_params = variational_mc.generate_cpds()
-    # models = variational_mc.fit_ELBO(data, fitted_mc)
-    models = variational_mc.fit_ELBO(data, mc)
-    return [(model.weights[0].weight.item(), model.weights[0].bias.item(), model.cov_matrix()) for model in models]
-    # import IPython
-    # IPython.embed()
+    variational_mc.generate_cpds()
+
+    # Uncomment to use p instead of p_hat for VI
+    # ========
+    # models = variational_mc.fit_ELBO(data, mc, plot_name="vi_loss_true")
+    # ========
+    
+    # Uncomment to use p_hat instead of p for VI
+    # ========
+    fitted_mc = SimpleMarkovChain(mc.num_nodes)
+    (x1_m, x1_sd), fp = fitted_mc.fit_cpds_to_data(data, log_fn)
+
+    plt.plot(list(a_errs.values())[0], label="Weight")
+    plt.plot(list(b_errs.values())[0], label="Bias")
+    plt.plot(list(sd_errs.values())[0], label="SD")
+    plt.legend()
+    plt.savefig("fitted_mc_errors.png")
+    plt.close()
+
+    models = variational_mc.fit_ELBO(data, mc, plot_name="vi_loss_fitted")
+    # ========
+
+    return models[0]
 
     # TODO compare with the true posterior distribution
 
 
 def compute_joint_linear(mc, num_samples=10000):
     # Only works for scalar linear gaussian
-    evidence = {"X_5": 1}
+    evidence = {"X_2": 1}
     query_node = "X_1"
-    evidence_node = "X_5"
+    evidence_node = "X_2"
 
     all_coeffs = []
     means = []
@@ -152,10 +172,8 @@ def compute_joint_linear(mc, num_samples=10000):
     prev_cov = 1
     all_cov = []
     for i, var in enumerate(mc.ordering):
-        print(f"{i}: Node {var}")
         node = mc.get_node(var)
         coeffs = node.cpd.linear_coeffs
-        print(f"CPD coeffs: {coeffs}")
         cov_node = node.cpd.sd**2
         all_coeffs.append(coeffs) #Putting all the coefficients together
         all_cov.append(cov_node)
@@ -164,13 +182,11 @@ def compute_joint_linear(mc, num_samples=10000):
         curr_mean = prev_mean * coeffs[0] + coeffs[1]
         means.append(curr_mean)
         prev_mean = curr_mean
-        print(f"Mean is now: {curr_mean}")
 
         # cov computations
         curr_cov = coeffs[0]*prev_cov*coeffs[0] + cov_node
         covs.append(curr_cov)
         prev_cov = curr_cov
-        print(f"Cov is now: {curr_cov}")
 
     # Getting the things we need for cross covariances
     query_idx = mc.ordering.index(query_node)
@@ -197,23 +213,59 @@ def compute_joint_linear(mc, num_samples=10000):
     cond_mean = query_mean + cross_cov*(1./evidence_cov)*(evidence[evidence_node] - evidence_mean)
     cond_cov = query_cov - cross_cov * (1. / evidence_cov) * cross_cov
 
-    print(f"query_cov: {query_cov}, cross_cov: {cross_cov}, evidence_cov: {evidence_cov}")
-    print(cond_cov)
+    # print(all_coeffs)
+    # print(all_cov)
+    c = [coeff[0] for coeff in all_coeffs[1:]]
+
+    # print(f"query_cov: {query_cov}, cross_cov: {cross_cov}, evidence_cov: {evidence_cov}")
+    # print(f"Numerator: {cross_cov ** 2}, should be: {(c[0] * c[1] * c[2]) ** 2 * all_cov[0] ** 2}")
+    # print(f"Denominator: {evidence_cov}, should be: {(c[0] * c[1] * c[2]) ** 2 * all_cov[0] + all_cov[3] + c[2]**2 * all_cov[2] + c[2]**2 * c[1]**2 * all_cov[1]}")
+    # print(cond_cov)
 
     return GaussianDistribution(cond_mean, np.sqrt(cond_cov))
 
 if __name__ == "__main__":
     print("Fitting experiments...")
-    mc = SimpleMarkovChain(5)
-    mc.generate_cpds()
-    # mc.specify_polynomial_cpds((0, 1), [(1, 0)], [1])
-    print("MC initialized")
-    vi_params = test_true_linear_variational(mc)
-    dis_params = test_true_linear_discriminative(mc)
-    true_dist = compute_joint_linear(mc)
-    # mc = SimpleMarkovChain(5)
-    # true_params = mc.specify_polynomial_cpds((0.5, 1), [(0.7, 0.36), (0.5, 0.9), (0.2, 0.05), (1.4, 0.6)], [0.5, 2.4, 0.8, 1.6])
-    # mc = SimpleMarkovChain(5)
-    # true_params = mc.generate_cpds()
-    # true_dist = compute_joint_linear(mc)
-    # print(true_dist.mean, true_dist.cov)
+
+    # Basic X_1 -> X_2
+    # Goal is to infer P(X_2 | X_1)
+    mc = SimpleMarkovChain(2)
+
+    ## More extreme example: should learn to do X1 = X2 - 1 for values near X2=2
+    ## X_1 ~ N(1, 0.0001)
+    ## X_2 | X_1 ~ N(X_1 + 1, 0.0001)
+    # mc.specify_polynomial_cpds((1, 0.0001), [(1, 1)], [0.0001])
+
+    mc.specify_polynomial_cpds((0, 1), [(1, 0)], [1])
+
+    # Exact inference to get a baseline
+    true_params = compute_joint_linear(mc)
+
+    # Amortized VI to approximate X_1 | X_2
+    vi_model = test_true_linear_variational(mc)
+    vi_params = vi_model.weights[0].weight.item(), vi_model.weights[0].bias.item(), vi_model.cov_matrix()
+
+    # Discriminative: learn X_1 | X_2 directly with MLE
+    disc_params = test_true_linear_discriminative(mc)
+
+    print(f"Exact inference: X1|X2 = N({true_params.mean} * X1 + 0, {true_params.cov ** 2})")
+    print(f"Discriminative: X1|X2 = N({disc_params[0]['a']} * X1 + {disc_params[0]['b']}, {disc_params[0]['sd'] ** 2}")
+    print(f"VI: X1|X2 = N({vi_params[0]} * X1 + {vi_params[1]}, {vi_params[2] ** 2})")
+
+
+    # ==========================
+    # TESTING VI RECONSTRUCTION
+    # ==========================
+
+    # # Try a range of evidence values from -1 to 1
+    # x_2_evidence = torch.tensor(np.linspace(-1, 1, 11))[:,None]
+
+    # # Run values through the encoder, q(X_1 | X_2)
+    # x_1_vi = vi_model([x_2_evidence])
+
+    # # Run through the decoder, p(X_2 | X_1)
+    # x_2_decoded = mc.get_node("X_2").cpd.cond_fn([x_1_vi.mean])
+
+    # print("Original values:", x_2_evidence)
+    # print(f"Encoder values: {x_1_vi.mean} with SD: {x_1_vi.cov}")
+    # print(f"Decoder values: {x_2_decoded.mean} with SD: {x_2_decoded.cov}")